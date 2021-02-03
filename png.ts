a508b1a9dab6b02f52cd01e1365c0f1865feca95
Lee Daniel Crocker <lee@ansync.com>
2021-02-02 17:15:10 -0800

    Still experimenting

--- png.ts
+++ png.ts
@@ -1,25 +1,45 @@
 
 export interface Chunk {
 	type: string;
-	size: number;
 	crc: number;
-	offset: number;
-	data: number;
-	next: number;
+	data: Uint8Array;
 }
 
-function isChunk(obj: any): obj is Chunk {
+enum ColorType {
+	Gray = 0,
+	Color = 2,
+	Indexed = 3,
+	GrayAlpha = 4,
+	ColorAlpha = 6
+};
+
+enum CompressionMethod {
+	Deflate = 0
+}
+
+enum FilterMethod {
+	ByLine = 0
+}
+
+enum InterlaceMethod {
+	None = 0,
+	Adam7 = 1
+}
+
+export function isChunk(obj: any): obj is Chunk {
     if ('string' !== typeof obj.type) return false;
-    if ('number' !== typeof obj.size) return false;
     if ('number' !== typeof obj.crc) return false;
-    if ('number' !== typeof obj.offset) return false;
-    if ('number' !== typeof obj.data) return false;
-    if ('number' !== typeof obj.next) return false;
+    if (! obj.data instanceof Uint8Array) return false;
     return true;
 }
 
 export interface HeaderChunk extends Chunk {
 	width: number;
 	height: number;
+	bitDepth: number;
+	colorType: ColorType;
+	compressionMethod: CompressionMethod;
+	filterMethod: FilterMethod;
+	interlaceMethod: InterlaceMethod;
 }
 
