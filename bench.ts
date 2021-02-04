196c6fdc019a1fa0b72392c2b8fa3be5ec1a72b2
Lee Daniel Crocker <lee@ansync.com>
2021-02-03 17:45:40 -0800

    More type-y stuff

--- bench.ts
+++ bench.ts
@@ -1,5 +1,6 @@
 import minimist from 'https://deno.land/x/deno_minimist@v1.0.2/mod.ts';
-import { Chunk } from './png.ts';
+import { BufReader } from 'https://deno.land/std/io/bufio.ts';
+import { TypeId, typeName, Chunk, InputStream as PNGFile } from './png.ts';
 
 const SCALE = 1. / 255;
 function byteToFloat(b: number): number { return SCALE * b; }
@@ -8,32 +9,6 @@ class Converter {
     private infile: string;
     private outfile: string;
     private baseName: string;
-
-    isPNG(bytes: Uint8Array): boolean {
-	const header = [ 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A ];
-
-	for (let i = 0; i < header.length; i += 1) {
-		if (bytes[i] !== header[i]) return false;
-	}
-	return true;
-    }
-
-    chunk(bytes: Uint8Array, offset: number): Chunk {
-	let result: any = { offset: offset };
-
-	result.size = (bytes[offset] << 24) + (bytes[offset + 1] << 16) +
-		(bytes[offset + 2] << 8) + bytes[offset + 3];
-	result.type = String.fromCharCode(bytes[offset + 4]) + String.fromCharCode(bytes[offset + 5]) +
-		String.fromCharCode(bytes[offset + 6]) + String.fromCharCode(bytes[offset + 7]);
-	result.data = offset + 8;
-
-	const cp = offset + 8 + result.size;
-	result.crc = (bytes[cp] << 24) + (bytes[cp + 1] << 16) + (bytes[cp + 2] << 8) + bytes[cp + 3];
-	result.next = cp + 4;
-
-	if (result.next > bytes.length) throw new Error('bad input');
-	return result as Chunk;
-    }
 
     constructor(f: string) {
         this.infile = f;
@@ -49,32 +24,15 @@ class Converter {
     async convert(): Promise<void> {
         console.log(`${this.infile} --> ${this.outfile}`);
 
-	const bytes = await Deno.readFile(this.infile);
-	console.log(`read ${bytes.length} bytes`);
+	const ps = new PNGFile();
+	await ps.open(new BufReader(await Deno.open(this.infile)));
 
-	if (this.isPNG(bytes)) {
-	    console.log(`input is PNG file`);
-	    let offset = 8;
-	    let type = '';
-
-	    do {
-		let chunk = this.chunk(bytes, offset);
-
-		console.log(`found ${chunk.type} (${chunk.size} bytes)`);
-		offset = chunk.next;
-		type = chunk.type;
-	    } while ('IEND' !== type);
+	for await (const chunk of ps.chunks()) {
+	    console.log(chunk.toString());
 	}
-
-	let result = new Float32Array(bytes.length);
-	for (let i = 0; i < bytes.length; i += 1) {
-		result[i] = byteToFloat(bytes[i]);
-	}
-
-	console.log(`${result[0]} ${result[1]} ${result[2]} ${result[42]}`);
     }
 }
-    
+
 class App {
     public verbose = false;
     public files: string[] = [];
--- png.ts
+++ png.ts
@@ -1,45 +1,204 @@
 
-export interface Chunk {
-	type: string;
-	crc: number;
-	data: Uint8Array;
+import { zlib, unzlib } from 'https://deno.land/x/denoflate/mod.ts';
+import { BufReader } from 'https://deno.land/std/io/bufio.ts';
+
+export const TypeId = {
+	IHDR:	0x49484452,
+	IDAT:	0x49444154,
+	IEND:	0x49454E44
 }
 
-enum ColorType {
+export const enum ColorType {
 	Gray = 0,
 	Color = 2,
 	Indexed = 3,
 	GrayAlpha = 4,
 	ColorAlpha = 6
 };
+function isColorType(n: number): boolean {
+	return (0 === n || 2 === n || 3 === n || 4 === n || 6 === n);
+}
+const colorTypeIndicators = [ 'GS', '??', 'TC', 'IN', 'GA', '??', 'CA' ];
 
-enum CompressionMethod {
+export const enum CompressionMethod {
 	Deflate = 0
 }
+function isCompressionMethod(n: number): boolean { return 0 === n; }
+const compressionMethodIndicators = [ 'Z' ];
 
-enum FilterMethod {
+export const enum FilterMethod {
 	ByLine = 0
 }
+function isFilterMethod(n: number): boolean { return 0 === n; }
+const filterMethodIndicators = [ 'L' ];
 
-enum InterlaceMethod {
+export const enum InterlaceMethod {
 	None = 0,
 	Adam7 = 1
 }
+function isInterlaceMethod(n: number): boolean { return (0 === n || 1 === n); }
+const interlaceMethodIndicators = [ 'N', '7' ];
+
+export interface Chunk {
+	type: number;
+	data: Uint8Array;
+}
+
+export class BaseChunk implements Chunk {
+	public type: number;
+	public data: Uint8Array;
+
+	constructor(t: number, d: Uint8Array) {
+		this.type = t;
+		this.data = d;
+	}
+
+	toString(): string {
+		return `${typeName(this.type)} (${this.data.length} bytes)`;
+	}
+}
+
+export class HeaderChunk extends BaseChunk {
+	public width: number;
+	public height: number;
+	public bitDepth: number;
+	public colorType: ColorType;
+	public compressionMethod: CompressionMethod;
+	public filterMethod: FilterMethod;
+	public interlaceMethod: InterlaceMethod;
+
+	constructor(	t: number,
+			d: Uint8Array,
+			w: number,
+			h: number,
+			b: number,
+			c: number,
+			z: number,
+			f: number,
+			i: number) {
+		super(t, d);
+		this.width = w;
+		this.height = h;
+		this.bitDepth = b;
+
+		if (! isColorType(c)) throw new TypeError(`${c} is not a known color type`);
+		if (! isCompressionMethod(z)) throw new TypeError(`${z} is not a known compression method`);
+		if (! isFilterMethod(z)) throw new TypeError(`${f} is not a known filter method`);
+		if (! isInterlaceMethod(i)) throw new TypeError(`${i} is not a known interlace method`);
+
+		this.colorType = c as ColorType;
+		this.compressionMethod = z as CompressionMethod;
+		this.filterMethod = f as FilterMethod;
+		this.interlaceMethod = i as InterlaceMethod;
+	}
+
+	toString(): string {
+		return `IHDR (${this.width} x ${this.height} x ${this.bitDepth} ${colorTypeIndicators[this.colorType]} ${compressionMethodIndicators[this.compressionMethod]} ${filterMethodIndicators[this.filterMethod]} ${interlaceMethodIndicators[this.interlaceMethod]})`;
+	}
+}
 
 export function isChunk(obj: any): obj is Chunk {
-    if ('string' !== typeof obj.type) return false;
-    if ('number' !== typeof obj.crc) return false;
-    if (! obj.data instanceof Uint8Array) return false;
+    if ('number' !== typeof obj.type) return false;
+    if (! obj.data) return false;
+    if (! (obj.data instanceof Uint8Array)) return false;
     return true;
 }
 
-export interface HeaderChunk extends Chunk {
-	width: number;
-	height: number;
-	bitDepth: number;
-	colorType: ColorType;
-	compressionMethod: CompressionMethod;
-	filterMethod: FilterMethod;
-	interlaceMethod: InterlaceMethod;
+export function typeName(id: number) {
+	return String.fromCharCode(0xFF & (id >> 24)) +
+		String.fromCharCode(0xFF & (id >> 16)) +
+		String.fromCharCode(0xFF & (id >> 8)) +
+		String.fromCharCode(0xFF & id);
 }
 
+export class InputStream {
+	private reader: BufReader | null = null;
+	private partial: any = null;
+
+	constructor() {
+	}
+
+	async open(r: BufReader): Promise<void> {
+		this.reader = r;
+		const h1 = await this.readUint32();
+		const h2 = await this.readUint32();
+		if (! (0x89504E47 === h1 && 0x0D0A1A0A === h2))
+			throw new Error('not a PNG file');
+	}
+
+	async readUint32(): Promise<number> {
+		if (null === this.reader)
+			throw new Error(`no open reader`);
+	    const b3 = await this.reader.readByte();
+	    const b2 = await this.reader.readByte();
+	    const b1 = await this.reader.readByte();
+	    const b0 = await this.reader.readByte();
+	    if (null === b3 || null === b2 || null === b1 || null === b0)
+		throw new Error(`read error`);
+
+	    let v = (b3 << 24) + (b2 << 16) + (b1 << 8) + b0;
+	    if (v < 0) v += 0x100000000;
+	    return v;
+	}
+
+	async nextChunk(): Promise<Chunk> {
+		if (null === this.reader)
+			throw new Error(`no open reader`);
+
+	    const size = await this.readUint32();
+	    const type = await this.readUint32();
+	    const data = new Uint8Array(size);
+
+	    if (0 !== size) { await this.reader.readFull(data); }
+	    let crc = await this.readUint32();
+	    // TODO: verify crc
+
+	    return new BaseChunk(type, data);
+	}
+
+	decode(cin: BaseChunk): Chunk {
+		if (TypeId.IHDR === cin.type) {
+			const width = (cin.data[0] << 24) + (cin.data[1] << 16) +
+				(cin.data[2] << 8) + cin.data[3];
+			const height = (cin.data[4] << 24) + (cin.data[4] << 16) +
+				(cin.data[6] << 8) + cin.data[7];
+			const b = cin.data[8];
+			const c = cin.data[9];
+			const z = cin.data[10];
+			const f = cin.data[11];
+			const i = cin.data[12];
+
+			const cout = new HeaderChunk(cin.type, cin.data, width, height, b, c, z, f, i);
+			return cout;
+		}
+		return cin;
+	}
+
+	async *chunks(): AsyncGenerator<Chunk> {
+	    let type = 0;
+	    let partial: Chunk | null = null;
+
+	    do {
+		let chunk = await this.nextChunk();
+		type = chunk.type;
+
+		if (TypeId.IDAT === chunk.type) {
+			if (null === partial) {
+				partial = chunk;
+			} else {
+				let p1 = partial.data;
+				partial.data = new Uint8Array(p1.length + chunk.data.length);
+				partial.data.set(p1);
+				partial.data.set(chunk.data, p1.length);
+			}
+			continue;
+		}
+		if (null !== partial && TypeId.IDAT !== chunk.type) {
+			partial.data = unzlib(partial.data);
+			yield this.decode(partial);
+			partial = null;
+		}
+		yield this.decode(chunk);
+	    } while (TypeId.IEND !== type);
+	}
+}
